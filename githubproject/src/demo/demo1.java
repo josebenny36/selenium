4f3422bb78cede0889fb1018dd296bc169d9b90c
jose <josebenny36@gmail.com>
2023-04-10 12:43:11 +0530

    adding numbers

--- githubproject/src/demo/demo1.java
+++ githubproject/src/demo/demo1.java
@@ -4,7 +4,9 @@ public class demo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int num1 = 5, num2 = 10, sum;
+	      sum = num1 + num2;
+	      System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
 	}
 
 }
